720f38f798105ad21daa56c8f111cfca0afcab11
Daniel Kolocka <dkolocka@sfu.ca>
2022-09-25 15:53:04 +0200

    Project imported to GitHub

---
+++ index.js
@@ -0,0 +1,29 @@
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+const map = L.map('myMap').setView([0, 0], 2);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+var marker = L.marker([0, 0]).addTo(map);
+let initialCall = true;
+async function getData() {
+    let response = await fetch(api_url);
+    let data = await response.json();
+    // console.log(data);
+    let { latitude, longitude, velocity, units } = data;
+    document.getElementById('latitude').textContent = latitude.toFixed(3);
+    document.getElementById('longitude').textContent = longitude.toFixed(3);
+    document.getElementById('velocity').textContent = velocity.toFixed(1);
+    document.getElementById('units').textContent = units;
+    marker.setLatLng([latitude, longitude]);
+    if (initialCall) {
+        map.setView([latitude, longitude], 3);
+        initialCall = !initialCall
+    }
+}
+
+getData();
+setInterval(getData, 1500);
